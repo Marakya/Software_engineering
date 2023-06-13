57720b6afc64ecc5c0c0c2a71b60f04eabf3b733
ekaterina_mayatskaya <mayatskaya.katya@mail.ru>
2023-06-13 21:43:13 +0500

    Add app to main_.py

--- main_.py
+++ main_.py
@@ -12,8 +12,34 @@ def predict_text(text):
         return text_sum(
             'Pink elephant likes eating a lot of fruits and vegetable, but it likes eating bananas more then others')
 
+
 def generate_image(text):
     if len(text) > 0:
         return picture_gen(text)
     else:
-        return picture_gen('Pink elephant')
+        return picture_gen('Pink elephant')   
+
+
+if __name__ == '__main__':
+    with gr.Blocks() as demo:
+        with gr.Row():
+            with gr.Column(scale=2):
+                pass
+            with gr.Column(scale=3, elem_id='main_col'):
+                with gr.Row():
+                    gr.Markdown(
+                        """### The meaning in one picture!""")
+                with gr.Row():
+                    text_input = gr.Text(label='Enter your text here:', elem_id='text_input')
+                    text_output = gr.Text(label='Text summary here', elem_id='text_output', interactive=False)
+                with gr.Row():
+                    get_text = gr.Button("Get summary", elem_id='button2')
+                    image_button = gr.Button("Generate picture", elem_id='button2')
+                with gr.Row():
+                    image_output = gr.Image(label='Result', show_label=True, elem_id='image')
+            with gr.Column(scale=2):
+                pass
+
+        get_text.click(predict_text, inputs=text_input, outputs=text_output)
+        image_button.click(generate_image, inputs=text_output, outputs=image_output)
+    demo.launch(server_name='0.0.0.0', share = True)
