6b26485142b42861c96710209c5d0bacbb6a62ef
ekaterina_mayatskaya <mayatskaya.katya@mail.ru>
2023-06-13 21:39:22 +0500

    Add main_.py

---
+++ main_.py
@@ -0,0 +1,13 @@
+import gradio as gr
+import numpy as np
+
+text_sum = gr.Blocks.load(name="huggingface/csebuetnlp/mT5_multilingual_XLSum")
+picture_gen = gr.Blocks.load(name="huggingface/CompVis/stable-diffusion-v1-4")
+
+
+def predict_text(text):
+    if len(text) > 0:
+        return text_sum(text)
+    else:
+        return text_sum(
+            'Pink elephant likes eating a lot of fruits and vegetable, but it likes eating bananas more then others')
