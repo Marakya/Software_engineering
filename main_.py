89bccf719352a96436eb70d5dfbfbc01f618277f
ekaterina_mayatskaya <mayatskaya.katya@mail.ru>
2023-06-13 21:44:07 +0500

    Add comments to main_.py

--- main_.py
+++ main_.py
@@ -4,7 +4,7 @@ import numpy as np
 text_sum = gr.Blocks.load(name="huggingface/csebuetnlp/mT5_multilingual_XLSum")
 picture_gen = gr.Blocks.load(name="huggingface/CompVis/stable-diffusion-v1-4")
 
-
+# Если юзер не ввел текст, то суммаризация происходит по дефолтному тексту
 def predict_text(text):
     if len(text) > 0:
         return text_sum(text)
@@ -12,13 +12,12 @@ def predict_text(text):
         return text_sum(
             'Pink elephant likes eating a lot of fruits and vegetable, but it likes eating bananas more then others')
 
-
+# Если юзер не ввел текст, то создание изображения происходит по дефолтному тексту
 def generate_image(text):
     if len(text) > 0:
         return picture_gen(text)
     else:
-        return picture_gen('Pink elephant')   
-
+        return picture_gen('Pink elephant')
 
 if __name__ == '__main__':
     with gr.Blocks() as demo:
