4171fd3218df86baeb4f4c3313f2ae2935a255d0
med-alex <med-alex@users.noreply.github.com>
2023-06-14 18:44:54 +0500

    add few tests

---
+++ test_main_.py
@@ -0,0 +1,19 @@
+import main_
+import imghdr
+import pytest
+
+
+def test_read_sum():
+    summ = main_.predict_text('Videos that say approved vaccines are dangerous and cause autism, \
+                            cancer or infertility are among those that will be taken down, the company said.  \
+                            The policy includes the termination of accounts of anti-vaccine influencers.  \
+                            Tech giants have been criticised for not doing more to counter false health \
+                            information on their sites.  In July, US President Joe Biden said social media \
+                            platforms were largely responsible for peoples scepticism in getting vaccinated \
+                            by spreading misinformation, and appealed for them to address the issue.')
+    assert type(summ) == str
+
+
+def test_kl():
+    result = main_.generate_image("")
+    assert imghdr.what(result) == "jpeg"
